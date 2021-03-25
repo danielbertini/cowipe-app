329ce57dbd38c66927efed30c7caa12395dda295
Daniel Bertini <danielbertini@gmail.com>
2021-03-25 18:42:00 -0300

    melhorias...

--- src/components/organisms/gifts/sent.js
+++ src/components/organisms/gifts/sent.js
@@ -1,30 +1,95 @@
-import React, { memo, useEffect, useState } from "react";
+import React, { memo, useEffect, useState, useCallback } from "react";
 import { useTranslation } from "react-i18next";
-import moment from "moment";
 import { DialogActions, DialogContent } from "@material-ui/core";
 
-import i18n from "../../../i18n";
-import Typography from "../../atoms/display/typography";
+import api from "../../../services/api";
+import Info from "../../atoms/display/info";
+import Snackbar from "../../atoms/feedback/snackbar";
+import LinearProgress from "../../atoms/feedback/linearProgress";
 
 const OrganismsGiftsSent = (props) => {
   const { t } = useTranslation();
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [snackbar, setSnackbar] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
+
+  const getData = useCallback(() => {
+    api({ method: "POST", url: `gifts/getSent` })
+      .then((response) => {
+        if (response.data.success) {
+          setLoading(false);
+          setData(response.data.result);
+        } else {
+          if (response.data.blocked) {
+            setSnackbarMessage(t("alerts.unavailableProfile"));
+            setSnackbar(true);
+          } else {
+            if (response.data.message) {
+              setLoading(false);
+              setSnackbarMessage(response.data.message);
+              setSnackbar(true);
+            }
+          }
+        }
+      })
+      .catch((error) => {
+        setSnackbarMessage(t("alerts.unavailableService"));
+        setSnackbar(true);
+      });
+  }, [t]);
+
+  useEffect(() => {
+    getData();
+  }, [getData]);
+
+  const renderContent = () => {
+    if (loading) {
+      return (
+        <>
+          <LinearProgress />
+        </>
+      );
+    } else {
+      if (data.length > 0) {
+      } else {
+        return (
+          <>
+            <Info text={t("alerts.noGiftsSent")} />
+          </>
+        );
+      }
+    }
+  };
 
   const renderComponent = () => {
     return (
       <>
-        <DialogContent></DialogContent>
-        <DialogActions
+        <DialogContent>{renderContent()}</DialogContent>
+        {/* <DialogActions
           style={{
             display: "flex",
             flexDirection: "columns",
             justifyContent: "space-between",
           }}
-        ></DialogActions>
+        ></DialogActions> */}
+        {snackbar && (
+          <Snackbar
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "center",
+            }}
+            open={snackbar}
+            onClose={() => setSnackbar(false)}
+            autoHideDuration={3000}
+            message={snackbarMessage}
+          />
+        )}
       </>
     );
   };
 
-  return renderComponent();
+  return props.display ? renderComponent() : <></>;
 };
 
 export default memo(OrganismsGiftsSent);
--- src/components/templates/dialogs/gifts.js
+++ src/components/templates/dialogs/gifts.js
@@ -1,11 +1,9 @@
-import React, { memo, useCallback, useState, useEffect } from "react";
+import React, { memo, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { isMobile } from "react-device-detect";
 import { useTheme } from "@material-ui/core/styles";
-import { Dialog, DialogContent, Tabs, Tab } from "@material-ui/core";
+import { Dialog, Tabs, Tab } from "@material-ui/core";
 
-import Snackbar from "../../atoms/feedback/snackbar";
-import LinearProgress from "../../atoms/feedback/linearProgress";
 import OrganismsGiftsSent from "../../organisms/gifts/sent";
 import OrganismsGiftsReceived from "../../organisms/gifts/received";
 import DialogTitle from "../dialogs/dialogTitle";
@@ -13,52 +11,11 @@ import DialogTitle from "../dialogs/dialogTitle";
 const TemplatesDialogsGifts = (props) => {
   const theme = useTheme();
   const { t } = useTranslation();
-  const [loading, setLoading] = useState(true);
-  const [connection, setConnection] = useState();
-  const [snackbar, setSnackbar] = useState(false);
-  const [snackbarMessage, setSnackbarMessage] = useState("");
-  const [profile, setProfile] = useState([]);
   const [tabValue, setTabValue] = useState(0);
-  const [room, setRoom] = useState();
 
   const handleTabChange = (event, newValue) => {
     setTabValue(newValue);
   };
-
-  const getProfile = useCallback(() => {
-    // api({ method: "GET", url: `user/profile/${props.data._id}` })
-    //   .then((response) => {
-    //     if (response.data.success) {
-    //       setLoading(false);
-    //       setProfile(response.data.profile);
-    //       setConnection(response.data.connection);
-    //       if (response.data.room) {
-    //         setRoom(response.data.room);
-    //       }
-    //     } else {
-    //       if (response.data.blocked) {
-    //         setSnackbarMessage(t("alerts.unavailableProfile"));
-    //         setSnackbar(true);
-    //         setTimeout(() => {
-    //           props.open(false);
-    //         }, 3000);
-    //       } else {
-    //         if (response.data.message) {
-    //           setLoading(false);
-    //           setSnackbarMessage(response.data.message);
-    //           setSnackbar(true);
-    //         }
-    //       }
-    //     }
-    //   })
-    //   .catch((error) => {
-    //     setSnackbarMessage(t("alerts.unavailableService"));
-    //     setSnackbar(true);
-    //     setTimeout(() => {
-    //       props.open(false);
-    //     }, 3000);
-    //   });
-  }, [props, t]);
 
   const renderTabs = () => {
     return (
@@ -78,10 +35,6 @@ const TemplatesDialogsGifts = (props) => {
       </>
     );
   };
-
-  useEffect(() => {
-    getProfile();
-  }, [getProfile]);
 
   return (
     <>
@@ -105,18 +58,6 @@ const TemplatesDialogsGifts = (props) => {
         <OrganismsGiftsSent display={tabValue === 0} />
         <OrganismsGiftsReceived display={tabValue === 1} />
       </Dialog>
-      {snackbar && (
-        <Snackbar
-          anchorOrigin={{
-            vertical: "bottom",
-            horizontal: "center",
-          }}
-          open={snackbar}
-          onClose={() => setSnackbar(false)}
-          autoHideDuration={3000}
-          message={snackbarMessage}
-        />
-      )}
     </>
   );
 };
