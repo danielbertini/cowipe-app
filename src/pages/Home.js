11d37829e396ebd3da8d847319227cff46e13f2e
Daniel Bertini <danielbertini@gmail.com>
2021-03-28 19:45:44 -0300

    melhorias...

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Container, Grid } from "@material-ui/core";
 
-const Home = () => {
-  return <></>;
+import MainMenu from "../components/organisms/menus/main";
+
+const PagesHome = () => {
+  return (
+    <>
+      <MainMenu />
+      <Container>
+        <Grid container direction="row" spacing={20} justify="center">
+          <Grid item xl={12} lg={12} md={12} sm={12} xs={12}></Grid>
+        </Grid>
+      </Container>
+    </>
+  );
 };
 
-export default Home;
+export default PagesHome;
