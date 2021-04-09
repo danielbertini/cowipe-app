475e5402aab6d0cafbf6f1e6cc6342fc7540d4f8
Daniel Bertini <danielbertini@gmail.com>
2021-04-09 01:08:39 -0300

    melhorias...

--- src/services/api.js
+++ src/services/api.js
@@ -1,17 +1,18 @@
 import axios from "axios";
-import i18n from '../i18n';
+import i18n from "../i18n";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API,
   headers: {
-    'Accept': 'application/json; charset=UTF-8',
-    'Content-Type': 'application/json; charset=UTF-8',
-  },  
+    Accept: "application/json; charset=UTF-8",
+    "Content-Type": "application/json; charset=UTF-8",
+  },
+  withCredentials: true,
+  baseURL: process.env.REACT_APP_API,
 });
 
 api.interceptors.request.use(
   (config) => {
-    config.headers['Accept-Language'] = i18n.language;
+    config.headers["Accept-Language"] = i18n.language;
     return config;
   },
   (error) => {
